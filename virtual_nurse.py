c0cdd63c578c374261bad06eabdd4106c63cfd76
Ishan Sheth <kevishan@gmail.com>
2024-02-28 01:05:26 -0500

    Update virtual_nurse.py

--- virtual_nurse.py
+++ virtual_nurse.py
@@ -190,7 +190,7 @@ st.title("Virtual Nurse Patient Assessment")
 url = "https://docs.google.com/document/d/1g63YfenbIJZXq9SG3l4kAcpLe9EOq126SGduGT4U5l4/edit?usp=sharing"
 link_text = "Click here to access the document for more information."
 
-if st.checkbox("I consent to filling out this assessment. " + f"[{link_text}]({url})"):
+if st.checkbox(f"[{link_text}]({url})"):
     display_chat_history()
 
     if st.session_state['current_question_index'] < len(st.session_state['initial_questions']):
